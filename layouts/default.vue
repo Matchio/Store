d2ceeb93be37069ef56ab3186b78c22304cdd9a5
Matchio <oaktana07@gmail.com>
2024-09-27 04:20:11 +0700

    awdaw

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,7 +18,7 @@
       </main>
       <footer class="bg-white py-4 mt-10 shadow-inner">
         <div class="container mx-auto text-center text-gray-600">
-          © 2024 My Portfoli o
+          © 2024 My Portfolio oak
         </div>
       </footer>
     </div>
