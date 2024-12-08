2d6c794328c543577139c4994cb8ccbafc9ae8d6
KoonPaT <40105170+KoonPaT@users.noreply.github.com>
2024-12-09 04:39:39 +0700

    upload phat profile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  css: [
+    'bootstrap/dist/css/bootstrap.css',
+    'bootstrap-icons/font/bootstrap-icons.css',
+  ],
+  build: {
+    transpile: ['bootstrap-vue-3'],
+  },
 })
